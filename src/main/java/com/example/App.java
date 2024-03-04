ecc69861015f6d3b678dcc42efa795db3bdb60cc
Brandon Anthony <brandon.chitay@ubiquo.io>
2024-03-03 19:03:42 -0600

    added(stacks): base code for stack excercise

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -6,16 +6,31 @@ import java.util.Random;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
 
     public static void main(String[] args) {
-        System.out.println("Arreglo original:");
+
+        String cadena1 = "[{()}]";
+        String cadena2 = "{[()]}";
+        String cadena3 = "(){}[{}]";
+        String cadena4 = "{[(])}";
+        String cadena5 = "[{]}";
 
         long inicio = System.currentTimeMillis();
+
+        System.out.println("Brandon Chitay Marzo 2024"); // Sustituir por el nombre del estudiante y la fecha
+
+        // Ejecución de la solución
+        System.out.println(EjercicioPilas.estaBalanceada(cadena1)); // true
+        System.out.println(EjercicioPilas.estaBalanceada(cadena2)); // true
+        System.out.println(EjercicioPilas.estaBalanceada(cadena3)); // true
+        System.out.println(EjercicioPilas.estaBalanceada(cadena4)); // false
+        System.out.println(EjercicioPilas.estaBalanceada(cadena5)); // false
+
         // Código a medir
         long fin = System.currentTimeMillis();
         // Fin del código a medir
+
         System.out.println("Tiempo de ejecución: " + (fin - inicio) + " milisegundos");
     }
 }
--- src/main/java/com/example/EjercicioPilas.java
+++ src/main/java/com/example/EjercicioPilas.java
@@ -3,19 +3,6 @@ package com.example;
 import java.util.Stack;
 
 public class EjercicioPilas {
-    public static void main(String[] args) {
-        String cadena1 = "[{()}]";
-        String cadena2 = "{[()]}";
-        String cadena3 = "(){}[{}]";
-        String cadena4 = "{[(])}";
-        String cadena5 = "[{]}";
-
-        System.out.println(estaBalanceada(cadena1)); // true
-        System.out.println(estaBalanceada(cadena2)); // true
-        System.out.println(estaBalanceada(cadena3)); // true
-        System.out.println(estaBalanceada(cadena4)); // false
-        System.out.println(estaBalanceada(cadena5)); // false
-    }
 
     public static boolean estaBalanceada(String cadena) {
         Stack<Character> pila = new Stack<>();
