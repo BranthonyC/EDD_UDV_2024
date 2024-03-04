38002f555cd1f589f5e2c35799759574f6a76b1e
Brandon Anthony <brandon.chitay@ubiquo.io>
2024-03-03 20:23:59 -0600

    added:(excercices for stacks and queues

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,6 +1,8 @@
 package com.example;
 
+import java.util.Queue;
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * Hello world!
@@ -10,27 +12,62 @@ public class App {
 
     public static void main(String[] args) {
 
-        String cadena1 = "[{()}]";
-        String cadena2 = "{[()]}";
-        String cadena3 = "(){}[{}]";
-        String cadena4 = "{[(])}";
-        String cadena5 = "[{]}";
-
         long inicio = System.currentTimeMillis();
 
-        System.out.println("Brandon Chitay Marzo 2024"); // Sustituir por el nombre del estudiante y la fecha
+        System.out.println("Brandon Chitay Marzo 2024");
 
         // Ejecución de la solución
-        System.out.println(EjercicioPilas.estaBalanceada(cadena1)); // true
-        System.out.println(EjercicioPilas.estaBalanceada(cadena2)); // true
-        System.out.println(EjercicioPilas.estaBalanceada(cadena3)); // true
-        System.out.println(EjercicioPilas.estaBalanceada(cadena4)); // false
-        System.out.println(EjercicioPilas.estaBalanceada(cadena5)); // false
-
+        EjercicioPilas.esPalindromo("NoEsPalindromo");
+        EjercicioPilas.esPalindromo("oso");
         // Código a medir
         long fin = System.currentTimeMillis();
         // Fin del código a medir
 
-        System.out.println("Tiempo de ejecución: " + (fin - inicio) + " milisegundos");
+        System.out.println("Tiempo de ejecución: " + (fin - inicio) + "milisegundos");
     }
+
+    // EJERCICIO Cola de amigos
+    // public static void main(String[] args) {
+    // Scanner scanner = new Scanner(System.in);
+
+    // System.out.println("Brandon Chitay Marzo Cola de amigos\n\n"); // Sustituir
+    // por el nombre
+    // System.out.println("Ingrese la cantidad de amigos:");
+    // int cantidadAmigos = scanner.nextInt();
+
+    // Queue<String> amigos = EjercicioPilas.colaDeAmigos(cantidadAmigos);
+
+    // System.out.println("\nAmigos en la cola:");
+    // while (!amigos.isEmpty()) {
+    // System.out.println(amigos.poll());
+    // }
+    // }
+
+    // public static void main(String[] args) {
+
+    // String cadena1 = "[{()}]";
+    // String cadena2 = "{[()]}";
+    // String cadena3 = "(){}[{}]";
+    // String cadena4 = "{[(])}";
+    // String cadena5 = "[{]}";
+
+    // long inicio = System.currentTimeMillis();
+
+    // System.out.println("Brandon Chitay Marzo 2024"); // Sustituir por el nombre
+
+    // // Ejecución de la solución
+    // System.out.println(cadena1 + " " + EjercicioPilas.estaBalanceada(cadena1));
+    // // true
+    // // System.out.println(EjercicioPilas.estaBalanceada(cadena2)); // true
+    // // System.out.println(EjercicioPilas.estaBalanceada(cadena3)); // true
+    // // System.out.println(EjercicioPilas.estaBalanceada(cadena4)); // false
+    // // System.out.println(EjercicioPilas.estaBalanceada(cadena5)); // false
+
+    // // Código a medir
+    // long fin = System.currentTimeMillis();
+    // // Fin del código a medir
+
+    // System.out.println("Tiempo de ejecución: " + (fin - inicio) +
+    // "milisegundos");
+    // }
 }
--- src/main/java/com/example/EjercicioPilas.java
+++ src/main/java/com/example/EjercicioPilas.java
@@ -1,14 +1,68 @@
 package com.example;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class EjercicioPilas {
 
     public static boolean estaBalanceada(String cadena) {
         Stack<Character> pila = new Stack<>();
-        // Debe desarrollar su respuesta aquí.
-        // Respuesta por defecto al no haber implementado su respuesta.
-        // Debe borrar esta línea al implementar su respuesta.
-        return false;
+        char[] characters = cadena.toCharArray();
+        for (int i = 0; i < characters.length; i++) {
+            if (characters[i] == '(' || characters[i] == '{' || characters[i] == '[') {
+                pila.push(characters[i]);
+            } else {
+                if (pila.isEmpty()) {
+                    return false;
+                }
+                if (characters[i] == ')' && pila.peek() == '(') {
+                    pila.pop();
+                } else if (characters[i] == '}' && pila.peek() == '{') {
+                    pila.pop();
+                } else if (characters[i] == ']' && pila.peek() == '[') {
+                    pila.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return pila.isEmpty();
+    }
+
+    public static boolean esPalindromo(String texto) {
+        LinkedList<Character> pila = new LinkedList<>();
+        LinkedList<Character> cola = new LinkedList<>();
+
+        for (char c : texto.toCharArray()) {
+            if (Character.isLetter(c)) {
+                c = Character.toLowerCase(c);
+                pila.push(c);
+                cola.add(c);
+            }
+        }
+
+        while (!pila.isEmpty() && !cola.isEmpty()) {
+            if (pila.pop() != cola.poll()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static Queue<String> colaDeAmigos(int cantidadAmigos) {
+        Scanner scanner = new Scanner(System.in);
+        Queue<String> cola = new LinkedList<>();
+
+        for (int i = 0; i < cantidadAmigos; i++) {
+            scanner.nextLine(); // Limpiar el buffer
+            System.out.println("Ingrese el nombre del amigo " + (i + 1) + ":");
+            cola.add(scanner.nextLine());
+        }
+        scanner.close();
+
+        return cola;
     }
 }
